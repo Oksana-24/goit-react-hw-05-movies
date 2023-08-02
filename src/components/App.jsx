cf72262ec845886ccfb352c20e54078cfa464449
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-02 20:39:15 +0200

    add new route and element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import SharedLayout from './SharedLayout/SharedLayout';
+
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movie/:id" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="review" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
+    </Routes>
   );
 };
